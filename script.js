3e1e392014c9532fc33d4089a177be2a692187bf
ElectrixCodes <84102668+ElectrixCodes@users.noreply.github.com>
2021-05-12 18:21:29 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,30 @@
 var canvas = document.getElementById("suit");
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.imageSmoothingEnabled = false; // disable interpolation when upscaling images
 
-//var img = ;
-//ctx.drawImage(img, 0, 0);
+var suit = "gravity";
+var pose = "front";
+var imageIndex = 0;
+
+var sprites = [];
+var stop = (pose == "morph_ball" ? 8 : 4);
+for(var i = 0; i < stop; i++) {
+	sprites.push(new Image());
+	sprites[i].src = "https://electrixcodes.github.io/AM2RSuitEditor/assets/sprites/" + suit + "/" + pose + "/" + i.toString() + ".png";
+}
+
+/*
+var img = new Image();
+img.onload = function() {
+	ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+}
+img.src = "https://electrixcodes.github.io/AM2RSuitEditor/assets/sprites/gravity/front/0.png";
+*/
+
+function suitLoop() {
+	ctx.drawImage(sprites[imageIndex], 0, 0, canvas.width, canvas.height);
+	imageIndex = (imageIndex + 1) % sprites.length;
+}
+
+setInterval(suitLoop, 500);
