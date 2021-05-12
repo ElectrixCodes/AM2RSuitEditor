e915fa6e15f3ca26b1ad362e6067665ee95549c6
ElectrixCodes <84102668+ElectrixCodes@users.noreply.github.com>
2021-05-12 16:43:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+// Handles the JavaScript for this project.
+
+var canvas = document.getElementById("suit");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//var img = ;
+//ctx.drawImage(img, 0, 0);
